44cf28b1d5d9f5b973259b1d3d4248c6eac6af1f
Maksim Chemel <maksim.chemel@intexsoft.by>
2020-10-25 02:00:01 +0300

    Added connection with telegram API, created bot

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log('test')
+const { Telegraf } = require('telegraf')
+
+const bot = new Telegraf('1386576345:AAEWUF8Q9S19oSv2klfGremmv2h0Z4DocIw')
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
